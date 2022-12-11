bce42aa1c4560e8662bcb7b2b17c2bc2b3a9500f
SemTK <93530443+SemTK@users.noreply.github.com>
2022-12-11 18:22:36 +0100

    adjust

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,20 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BankSysteem</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"> 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
     @vite(['resources/css/style.css', 'resources/js/app.js'])
 </head>
 
 <body>
     <header>
-        @extends('components.header')    
+        @include('components.header')    
     </header>
     <main>
         @yield('content')    
     </main>
     <footer>
-        @extends('components.footer')    
+        @include('components.footer')    
     </footer>
 </body>
 </html>
