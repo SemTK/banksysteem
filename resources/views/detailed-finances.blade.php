1bdcf25fb408493942d5710c2dc7747d0d817cb8
SemTK <93530443+SemTK@users.noreply.github.com>
2022-12-14 14:09:08 +0100

    added bankaccounts coming up on detailed finances page

--- resources/views/detailed-finances.blade.php
+++ resources/views/detailed-finances.blade.php
@@ -4,6 +4,39 @@
 
 <div class="container">
     <h1>This is your current financial situation.</h1>
+    
+    <div class="finances">
+        <div class="finances-bankaccounts">
+            <h6>Here are your bankaccounts.</h6>
+            @foreach($bankaccounts as $bankaccount)
+            
+            @if($bankaccount->owner_id == Auth::user()->id)
+            
+            <div class="card" style="width: 18rem;">
+                <div class="card-header">
+                    Type: {{$bankaccount->type}}
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Balance: <span>{{$bankaccount->money}}</span></li>
+                    <li class="list-group-item">Yearly fee: <span>...</span></li>
+                    <li class="list-group-item">Created at: <span>{{$bankaccount->created_at}}</span></li>
+                    <li class="list-group-item">
+                        <div class="finances-money-buttons">
+                            <a class="btn btn-success" href="">Deposit</a>
+                            <a class="btn btn-danger" href="">Withdraw</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            
+            
+            @else
+            <p>You do not own any bankaccounts.</p>
+            @endif
+            
+            @endforeach
+        </div>
+    </div>
 </div>
 
 @endsection
