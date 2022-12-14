9ccc26733557e6f20139e5b94aa6f26e9c2ed6ae
SemTK <93530443+SemTK@users.noreply.github.com>
2022-12-14 14:07:59 +0100

    created migration

---
+++ database/migrations/2022_12_13_214622_create_bankaccounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bankaccounts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('owner_id');
+            $table->string('owner_name');
+            $table->boolean('adult')->default(true);
+            $table->bigInteger('money')->nullable();
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bankaccounts');
+    }
+};
