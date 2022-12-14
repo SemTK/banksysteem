71d930082a9ef0835b989c46948865bd49303c68
SemTK <93530443+SemTK@users.noreply.github.com>
2022-12-14 14:08:24 +0100

    added function to admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,6 +4,14 @@
 
 <div class="container">
     <h1>Here are all the admin functions available.</h1>
+    
+    <h4>Functions &darr;</h4>
+    <div class="admin-functions-grid">
+        <div class="admin-delete-account">
+            <p>Delete a bankaccount.</p>
+            <a class="btn btn-danger" href="{{ route('admin-delete-bankaccount-page') }}">&rarr; Delete a bankaccount</a>
+        </div>
+    </div>
 </div>
 
 @endsection
